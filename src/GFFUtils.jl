1301af4a13ad458512c61f381eefb58201c5b438
yangrui <2413667864@qq.com>
2024-01-22 17:48:01 +0800

    export faidx_read & bed_read

--- src/GFFUtils.jl
+++ src/GFFUtils.jl
@@ -9,7 +9,7 @@ using DataFramesMeta
 using NaturalSort
 import Base: show
 
-export GFFBox, gff_read, bed_write, show, tss, tes, region, bed_clip, bed_sort
+export GFFBox, gff_read, bed_write, show, tss, tes, region, bed_clip, bed_sort, faidx_read, bed_read
 
 include("gff_io.jl")
 include("gff_utils.jl")
